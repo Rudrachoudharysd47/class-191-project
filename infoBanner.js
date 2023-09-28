4a806c4bd156edc01ab22b0ce07933ad308796d5
Rudrachoudharysd47 <71619072+Rudrachoudharysd47@users.noreply.github.com>
2023-09-28 07:16:21 +0530

    Add files via upload

---
+++ infoBanner.js
@@ -0,0 +1,102 @@
+AFRAME.registerComponent("info-banner", {
+    schema: {
+      itemId: { default: "", type: "string" },
+    },
+    update: function () {
+      this.createBanner();
+    },
+    createBanner: function () {
+      postersInfo = {
+        superman: {
+          banner_url: "./Assets/MauriyanEmpire.jpg",
+          title: "Mauriyan Empire",
+          description:
+            "The Mauryan Empire (322 BCE - 185 BCE) supplanted the earlier Magadha Kingdom to assume power over large tracts of eastern and northern India. At its height, the empire stretched over parts of modern Iran and almost the entire Indian subcontinent, barring only the southern peninsular tip.",
+        },
+        spiderman: {
+          banner_url: "./Assets/British Rule.jpg",
+          title: "British Rule",
+          description:
+            "The British Empire was composed of the dominions, colonies, protectorates, mandates, and other territories ruled or administered by the United Kingdom and its predecessor states. It began with the overseas possessions and trading posts established by England in the late 16th and early 17th centuries.",
+        },
+        "captain-aero": {
+          banner_url: "./assets/posters/captain-aero-banner.jpg",
+          title: "Captain Aero",
+          released_year: "1942",
+          description:
+            "Modern Hinduism grew from the ancient texts called Vedas, and bore much similarity to other Indo-European religions like Zoroastrianism, incorporating strong elements of nature gods and their worship. Vedic Hinduism had spread all over the Indian subcontinent by the 4th century BC, assimilating elements of all local religious beliefs and practices.",
+        },
+      };
+      const { itemId } = this.data;
+  
+      const fadeBackgroundEl = document.querySelector("#fadeBackground");
+  
+      const entityEl = document.createElement("a-entity");
+      entityEl.setAttribute("visible", true);
+      entityEl.setAttribute("id", `${itemId}-banner`);
+  
+      entityEl.setAttribute("geometry", {
+        primitive: "plane",
+        width: 0.9,
+        height: 1,
+      });
+  
+      entityEl.setAttribute("material", { color: "#000" });
+      entityEl.setAttribute("position", { x: 0, y: 0.1, z: -1 });
+  
+      const item = postersInfo[itemId];
+  
+      const imageEl = this.createImageEl(item);
+      const titleEl = this.createTitleEl(item);
+      const descriptionEl = this.createDescriptionEl(item);
+  
+      entityEl.appendChild(imageEl);
+      entityEl.appendChild(titleEl);
+      entityEl.appendChild(descriptionEl);
+  
+      fadeBackgroundEl.appendChild(entityEl);
+    },
+    createImageEl: function (item) {
+      const entityEl = document.createElement("a-entity");
+      entityEl.setAttribute("visible", true);
+      entityEl.setAttribute("geometry", {
+        primitive: "plane",
+        width: 0.85,
+        height: 0.4,
+      });
+      entityEl.setAttribute("material", { src: item.banner_url });
+      entityEl.setAttribute("position", { x: 0, y: 0.3, z: 0.05 });
+      return entityEl;
+    },
+    createTitleEl: function (item) {
+      const entityEl = document.createElement("a-entity");
+      entityEl.setAttribute("visible", true);
+      entityEl.setAttribute("text", {
+        shader: "msdf",
+        anchor: "left",
+        font: "https://cdn.aframe.io/examples/ui/Viga-Regular.json",
+        width: 1.2,
+        height: 2,
+        color: "#fff",
+        value: `${item.title} (${item.released_year})`,
+      });
+      entityEl.setAttribute("position", { x: -0.4, y: 0.02, z: 0.05 });
+      return entityEl;
+    },
+    createDescriptionEl: function (item) {
+      const entityEl = document.createElement("a-entity");
+      entityEl.setAttribute("visible", true);
+      entityEl.setAttribute("text", {
+        shader: "msdf",
+        anchor: "left",
+        font: "https://cdn.aframe.io/examples/ui/Viga-Regular.json",
+        width: 0.75,
+        height: 2,
+        color: "#fff",
+        wrapCount: "40",
+        value: item.description,
+      });
+      entityEl.setAttribute("position", { x: -0.4, y: -0.24, z: 0.05 });
+      return entityEl;
+    },
+  });
